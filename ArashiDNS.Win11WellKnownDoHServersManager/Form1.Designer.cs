5461fc1704f4a7b25048d7189d1cecb1d02bbd3c
Milkey Tan <819774989@qq.com>
2022-01-17 18:05:28 +0800

    添加项目文件。

---
+++ ArashiDNS.Win11WellKnownDoHServersManager/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ArashiDNS.Win11WellKnownDoHServersManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnIP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnUrl = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnIP,
+            this.columnUrl});
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(12, 13);
+            this.listView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(858, 527);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnIP
+            // 
+            this.columnIP.Tag = "";
+            this.columnIP.Text = "IP Address";
+            this.columnIP.Width = 150;
+            // 
+            // columnUrl
+            // 
+            this.columnUrl.Text = "URL Template";
+            this.columnUrl.Width = 700;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.listView);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "ArashiDNS WellKnown DoH Servers Manager for Windows 11";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnIP;
+        private System.Windows.Forms.ColumnHeader columnUrl;
+    }
+}
+
---
+++ ArashiDNS.Win11WellKnownDoHServersManager/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ArashiDNS.Win11WellKnownDoHServersManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var key =
+                Registry.LocalMachine.OpenSubKey(
+                    "SYSTEM\\CurrentControlSet\\Services\\Dnscache\\Parameters\\DohWellKnownServers");
+
+            foreach (var item in key.GetSubKeyNames())
+            {
+                var li = new ListViewItem {Text = item};
+                li.SubItems.Add(key.OpenSubKey(item).GetValue("Template").ToString());
+                listView.Items.Add(li);
+            }
+        }
+    }
+}
